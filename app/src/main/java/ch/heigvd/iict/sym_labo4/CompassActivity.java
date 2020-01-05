00fd3d92ba2058c8995e65c95b4fb75c3efdc6c8
Pierrick Muller <pierrick.muller@heig-vd.ch>
2020-01-05 12:21:19 +0100

    Suppression TODO

--- app/src/main/java/ch/heigvd/iict/sym_labo4/CompassActivity.java
+++ app/src/main/java/ch/heigvd/iict/sym_labo4/CompassActivity.java
@@ -95,12 +95,4 @@ public class CompassActivity extends AppCompatActivity implements SensorEventLis
         // On ne s'occupe pas de la precision
     }
 
-    /* TODO
-        your activity need to register to accelerometer and magnetometer sensors' updates
-        then you may want to call
-        this.opglr.swapRotMatrix()
-        with the 4x4 rotation matrix, everytime a new matrix is computed
-        more information on rotation matrix can be found on-line:
-        https://developer.android.com/reference/android/hardware/SensorManager.html#getRotationMatrix(float[],%20float[],%20float[],%20float[])
-    */
 }
